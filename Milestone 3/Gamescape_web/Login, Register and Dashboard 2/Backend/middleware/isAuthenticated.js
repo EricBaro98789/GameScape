b1a2511e3cf43ca72455a709d5f81f7fe8833c67
EricBaro98789 <jkgdvdg@gmail.com>
2025-06-12 04:51:46 +0000

    authenticate bug fixing

--- Milestone 3/Gamescape_web/Login, Register and Dashboard 2/Backend/middleware/isAuthenticated.js
+++ Milestone 3/Gamescape_web/Login, Register and Dashboard 2/Backend/middleware/isAuthenticated.js
@@ -1,5 +1,5 @@
 module.exports = (req, res, next) => {
-    if (req.session.user && req.session.user.isAdmin) {
+    if (req.session && req.session.user) {
         next();
     } else {
         res.status(403).json({ error: 'Forbidden: Admin access required.' });
