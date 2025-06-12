017dfdd8fa4f198f81a2537b13a2949577c80cfc
EricBaro98789 <jkgdvdg@gmail.com>
2025-06-12 13:07:08 +0000

    Clean folders

--- Milestone 3/Gamescape_web/Login, Register and Dashboard 2/Backend/middleware/isAuthenticated.js
+++
@@ -1,8 +0,0 @@
-module.exports = (req, res, next) => {
-    if (req.session && req.session.user) {
-        next();
-    } else {
-        res.status(403).json({ error: 'Forbidden: Admin access required.' });
-    }
-};
-
